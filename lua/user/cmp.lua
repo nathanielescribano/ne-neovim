0a1c479a4739ab6795684b4f537cc7211d827873
nathaniel escribano <nathaniel@digit.co>
2022-04-13 09:28:36 -0700

    updating cmp deprecation

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -122,8 +122,14 @@ cmp.setup {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
   },
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  -- below is what i had before, if i want it back, i can remove the line with 'native'
+  -- window = {
+  --   documentation = {
+  --     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  --   }
+  -- },
+  window = {
+    documentation = "native",
   },
   experimental = {
     ghost_text = false,
